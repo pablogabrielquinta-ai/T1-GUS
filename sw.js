38dd4ffcbf333baf32d8b7835cf1bbcb11200d03
pablogabrielquinta-ai <pablogabrielquinta@gmail.com>
2026-08-29 09:55:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker mínimo: cachea la app para que abra offline
    y sirva desde caché cuando no hay conexión. */
-var CACHE_NAME = 'gu-t1-cache-v29';
+var CACHE_NAME = 'gu-t1-cache-v30';
 var APP_SHELL = [
   './',
   './index.html',
